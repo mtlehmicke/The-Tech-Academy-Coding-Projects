82ac131878cf57d9fd27f4edb77f5f998dca33e7
Max Lehmicke <mtlehmicke1@msn.com>
2020-02-16 15:59:53 -0800

    Add files via upload

---
+++ ACME Welcome Page.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
+        }
+    }
+}
