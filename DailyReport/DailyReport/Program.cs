828c71d6a235c7defecfd3343ff976f87db437e6
Max Lehmicke <mtlehmicke1@msn.com>
2020-02-19 19:15:46 -0800

    Update Program.cs

--- DailyReport/DailyReport/Program.cs
+++ DailyReport/DailyReport/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace DailyReport
 {
@@ -6,33 +6,27 @@ namespace DailyReport
     {
         static void Main()
         {
-            string whatCourse;
-            string pageNumber;
-            bool needHelp;
-            string positiveExperiences;
-            string courseFeedback;
-            string studyHours;
 
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
 
             Console.WriteLine("What course are you on?");
-            whatCourse = Console.ReadLine();
+            string whatCourse = Console.ReadLine();
 
             Console.WriteLine("What page number?");
-            pageNumber = Console.ReadLine();
+            string pageNumber = Console.ReadLine();
 
             Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
-            needHelp = bool.Parse(Console.ReadLine());
+            bool needHelp = bool.Parse(Console.ReadLine());
 
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            positiveExperiences = Console.ReadLine();
+            string positiveExperiences = Console.ReadLine();
 
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            courseFeedback = Console.ReadLine();
+            string courseFeedback = Console.ReadLine();
 
             Console.WriteLine("How many hours did you study today?");
-            studyHours = Console.ReadLine();
+            string studyHours = Console.ReadLine();
 
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.Have a great day!");
         }
