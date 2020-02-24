5d2dd12314f65ad311224adc6a122fd2da2cc883
Max Lehmicke <mtlehmicke1@msn.com>
2020-02-23 16:33:24 -0800

    Add files via upload

---
+++ WhileDoWhileApp/WhileDoWhileApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileDoWhileApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 11);
+
+            while (i < 100)
+            {
+                Console.WriteLine(i);
+                i = i + 5;
+                Console.ReadLine();
+            }
+        }
+    }
+}
