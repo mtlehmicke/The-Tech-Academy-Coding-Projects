5942dceeefcbbd1993a9396f59008544dd28535e
Max Lehmicke <mtlehmicke1@msn.com>
2020-03-23 14:11:55 -0700

    Add files via upload

---
+++ MethodApp/MethodApp/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodApp
+{
+    class Class1
+    {
+        public static int theInteger = Convert.ToInt32(Console.ReadLine());
+
+        public static void MethodOne()
+        {
+            Console.WriteLine(theInteger + " multiplied by ten equals:");
+            Console.WriteLine(theInteger * 10);
+        }
+
+        public static void MethodTwo()
+        {
+            Console.WriteLine(theInteger + " plus fifty equals:");
+            Console.WriteLine(theInteger + 50);
+        }
+
+        public static void MethodThree()
+        {
+            Console.WriteLine(theInteger + " divided by three equals:");
+            Console.WriteLine(theInteger / 3);
+        }
+    }
+}
---
+++ MethodApp/MethodApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MethodApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter an integer for this program to perform math operations on:");
+            Class1.MethodOne();
+            Class1.MethodTwo();
+            Class1.MethodThree();
+        }
+    }
+}
