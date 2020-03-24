3d01c52a1afa519022ba719870a183952594a819
Max Lehmicke <mtlehmicke1@msn.com>
2020-03-24 01:14:11 -0700

    Add files via upload

---
+++ PersonClassApp/PersonClassApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PersonClassApp
+{
+    class Person
+    {
+        public string FirstName = "Max";
+        public string LastName = "Lehmicke";
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+
+    class Employee : Person
+    {
+        int ID;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee newEmp = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            newEmp.SayName();
+        }
+    }
+}
