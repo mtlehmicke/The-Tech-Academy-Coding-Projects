c80740dffdfe8493e418d49a8d63803e1ee4419c
Max Lehmicke <mtlehmicke1@msn.com>
2020-03-24 16:02:23 -0700

    Add files via upload

---
+++ QuitTableApp/QuitTableApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QuitTableApp
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("The employee has quit the company.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee heQuits = new Employee();
+            heQuits.Quit();
+        }
+    }
+}
