d59cf12936061ad5acefe6410b2974a4905dbccf
Max Lehmicke <mtlehmicke1@msn.com>
2020-03-24 13:38:02 -0700

    Add files via upload

---
+++ PersonSayName/PersonSayName/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PersonSayName
+{
+    abstract class Person
+    {
+        public string FirstName;
+        public string LastName;        
+    }
+
+    class Employee : Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee newEmp = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            newEmp.SayName();
+        }
+    }
+}
