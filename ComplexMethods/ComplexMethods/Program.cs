85232fd452db181a0e78b320554d2f1ff8c572ba
Max Lehmicke <mtlehmicke1@msn.com>
2020-03-24 00:26:11 -0700

    Add files via upload

---
+++ ComplexMethods/ComplexMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComplexMethods
+{
+    class Program
+    {
+        static class firstClass
+        {
+            public static void DivMethod(double x)
+            {
+                Console.WriteLine(x / 2);
+            }
+            public static void DivMethod(int x, out int y, out int z)
+            {
+                y = 10;
+                z = 3;
+                Console.WriteLine(x * y * z);
+            }
+        }
+        static void Main()
+        {
+            double theDouble = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Entern an integer:");
+            firstClass.DivMethod(theDouble);
+        }
+    }
+}
