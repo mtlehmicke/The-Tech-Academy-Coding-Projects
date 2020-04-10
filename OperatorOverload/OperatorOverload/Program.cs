05d776575e03d10438824f379ebc26afed270aec
Max Lehmicke <mtlehmicke1@msn.com>
2020-04-09 22:13:48 -0700

    Add files via upload

---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OperatorOverload
+{
+    class Person
+    {
+        public string FirstName = "Max";
+        public string LastName = "Lehmicke";
+    }
+
+    class Employee : Person
+    {
+        public int ID = 12345;
+
+        public static bool operator ==(Employee leftID, Employee rightID)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Employee leftID, Employee rightID)
+        {
+            return !(leftID == rightID);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee defaultEmp = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                ID = 12345
+            };
+
+            Employee newEmp = new Employee()
+            {
+                FirstName = "Example",
+                LastName = "Student",
+                ID = 12345
+            };
+
+        }
+    }
+}
