2dfd606bd46bea67b3c48aa4922fd1f5823b0b8c
Max Lehmicke <mtlehmicke1@msn.com>
2020-04-25 15:41:49 -0700

    Add files via upload

--- AgeDateCheck/AgeDateCheck/Program.cs
+++ AgeDateCheck/AgeDateCheck/Program.cs
@@ -1,30 +1,44 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AgeDateCheck
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("How many years old are you?");
+
             try
             {
                 int TheAge = Convert.ToInt32(Console.ReadLine());
+
                 if (TheAge == 0)
                 {
-                    throw new Exception("You are not a newborn, enter your real birth year!");
+                    throw new ArgumentNullException();
                 }
                 if (TheAge < 0)
                 {
-                    throw new Exception("You are not from the future, enter your real birth year!");
+                    throw new ArgumentException();
                 }
+
                 int TheYear = 2020 - TheAge;
-                
+
                 Console.WriteLine("You were born in the year " + TheYear + ".");
             }
-            catch(Exception)
+            catch (ArgumentNullException)
             {
-                Console.WriteLine("Something went wrong.")
+                Console.WriteLine("You are not a newborn, enter your real birth year!");
             }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You are not from the future, enter your real birth year!");
+            }
+            catch (Exception)
+            {
+                    Console.WriteLine("Something went wrong.");
+            }
+            
         }
     }
 }
