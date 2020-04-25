d1eb7a4334c634149a5075fdc6b919de58ed13f7
Max Lehmicke <mtlehmicke1@msn.com>
2020-04-24 19:19:37 -0700

    Add files via upload

---
+++ AgeDateCheck/AgeDateCheck/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AgeDateCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int TheAge = Convert.ToInt32(Console.ReadLine());
+                if (TheAge == 0)
+                {
+                    throw new Exception("You are not a newborn, enter your real birth year!");
+                }
+                if (TheAge < 0)
+                {
+                    throw new Exception("You are not from the future, enter your real birth year!");
+                }
+                int TheYear = 2020 - TheAge;
+                
+                Console.WriteLine("You were born in the year " + TheYear + ".");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Something went wrong.")
+            }
+        }
+    }
+}
